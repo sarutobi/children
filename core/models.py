11b08b17ff97d8f6d94d03c9ca99cd3ecadecc30
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-18 18:25:21 +0400

    Fixed unicode in core models

--- core/models.py
+++ core/models.py
@@ -7,8 +7,15 @@ class GroupOfInterest(models.Model):
     name = models.CharField(max_length=40, verbose_name=('Наименование'))
 #    icon = models.ImageField(blank=True, null=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Interest(models.Model):
     name = models.CharField(max_length=40, verbose_name=('Наименование'))
     root = models.ForeignKey(GroupOfInterest)
 #    icon = models.ImageField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+)
