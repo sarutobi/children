83d7d51bcf625c4b627160742501fa08e2841abf
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-18 18:09:47 +0400

    Added core models

---
+++ core/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class GroupOfInterest(models.Model):
+    name = models.CharField(max_length=40, verbose_name=('Наименование'))
+#    icon = models.ImageField(blank=True, null=True)
+
+
+class Interest(models.Model):
+    name = models.CharField(max_length=40, verbose_name=('Наименование'))
+    root = models.ForeignKey(GroupOfInterest)
+#    icon = models.ImageField(blank=True, null=True)
