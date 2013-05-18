7360afdee88efb1211d5a4538118b14f684de201
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-18 20:33:38 +0400

    Added fake index view

---
+++ core/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index(request):
+    return render_to_response(
+        'loginform.html',
+        context_instance=RequestContext(request))
