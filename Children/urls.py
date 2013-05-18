6948f52588b04a34c197a9bba67e1e8b9ecf57ca
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-18 12:43:09 +0400

    Initial commit

---
+++ Children/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Children.views.home', name='home'),
+    # url(r'^Children/', include('Children.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
