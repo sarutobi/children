2c11b21bfaa29327f7c893d622f9be56cc751ca2
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-19 11:40:22 +0400

    Added admin organization list fields

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from .models import GroupOfInterest, Interest
+from .models import (
+    GroupOfInterest, Interest,
+    Organization, Workshop)
 
 
 class AdminGroupOfInterest(admin.ModelAdmin):
     pass
 
+admin.site.register(GroupOfInterest, AdminGroupOfInterest)
+
 
 class AdminInterest(admin.ModelAdmin):
     list_display = ('name', 'root')
 
-admin.site.register(GroupOfInterest, AdminGroupOfInterest)
 admin.site.register(Interest, AdminInterest)
+
+
+class AdminOrganization(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+
+admin.site.register(Organization, AdminOrganization)
