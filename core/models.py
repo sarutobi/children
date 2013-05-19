70bc8d1f9c3eb6b6d74f140cba2a1a6b2b97ef37
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-19 09:33:07 +0400

    Added meta class to core models

--- core/models.py
+++ core/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class GroupOfInterest(models.Model):
+    class Meta:
+        verbose_name = 'Группа интересов'
+        verbose_name_plural = 'Группы интересов'
+
     name = models.CharField(max_length=40, verbose_name=('Наименование'))
 #    icon = models.ImageField(blank=True, null=True)
 
@@ -12,6 +16,10 @@ class GroupOfInterest(models.Model):
 
 
 class Interest(models.Model):
+    class Meta:
+        verbose_name = 'Занятие'
+        verbose_name_plural = 'Занятия'
+
     name = models.CharField(max_length=40, verbose_name=('Наименование'))
     root = models.ForeignKey(GroupOfInterest)
 #    icon = models.ImageField(blank=True, null=True)
