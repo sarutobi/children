e3d0e64dce4a5d2a7ccd26417b7410968c984105
Valery Ilychev (Sarutobi) <ilychev@gmail.com>
2013-05-19 18:54:53 +0400

    Added rating forms for skills

--- users/admin.py
+++ users/admin.py
@@ -3,8 +3,14 @@
 from django.contrib import admin
 
 from .models import Skill
+from core.models import InteresToSkill
+
+
+class AdminSkillRating(admin.TabularInline):
+    model = InteresToSkill
+
 
 class AdminSkill(admin.ModelAdmin):
-    pass
+    inlines = [AdminSkillRating, ]
 
 admin.site.register(Skill, AdminSkill)
